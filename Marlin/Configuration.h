1347024c127d413ac975c7af46e57c7a78f996a1
Borys Yermakov <borys.yermakov@zorallabs.com>
2019-11-26 14:05:35 +0200

    change max temps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -444,13 +444,13 @@
 // Above this temperature the heater will be switched off.
 // This can protect components from overheating, but NOT from shorts and failures.
 // (Use MINTEMP for thermistor short/failure protection.)
-#define HEATER_0_MAXTEMP 255
-#define HEATER_1_MAXTEMP 255
-#define HEATER_2_MAXTEMP 255
-#define HEATER_3_MAXTEMP 255
-#define HEATER_4_MAXTEMP 255
-#define HEATER_5_MAXTEMP 255
-#define BED_MAXTEMP      120
+#define HEATER_0_MAXTEMP 250
+#define HEATER_1_MAXTEMP 250
+#define HEATER_2_MAXTEMP 250
+#define HEATER_3_MAXTEMP 250
+#define HEATER_4_MAXTEMP 250
+#define HEATER_5_MAXTEMP 250
+#define BED_MAXTEMP      95
 
 //===========================================================================
 //============================= PID Settings ================================
